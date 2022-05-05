d9a9962d69ec0ca328886eff2b8eacfc5c031e33
Rimberse <kerimpltm@gmail.com>
2022-05-05 16:06:34 +0200

    Change 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,46 @@
-console.log("Hello World!");
+define
+(
+    'Widget/scripts/script',
+    [
+        // 3DEXPERIENCE Cloud Platform JS modules 
+        'DS/PlatformAPI/PlatformAPI'
+    ],
+    function
+    (
+        PlatformAPI
+    )
+    {
 
-// Loading other modules within the main script
-requirejs(["https://rimberse.github.io/Widget/helper/util"], function(util) {
-    widget.body.innerHTML = "Redacted!";
-});
+        // Declare public functions or variables here. Accessible by other modules. Call it by "Main.<function>". Usage sample, e.g. Main.onLoad() 
+        var exports = {
+            onLoad: function() {
+                console.info("Global var widget", widget);
+
+                console.log("Hello World!");
+                widget.body.innerHTML = "Redacted!";
+            },
+
+            onResize: function() {
+                console.info("onResize");
+            },
+
+            onViewChange: function() {
+                console.info("onViewChange");
+            },
+
+            onEdit: function() {
+                console.info("onEdit");
+            },
+
+            onRefresh: function() {
+                console.info("onRefresh");
+            },
+
+            endEdit: function() {
+                console.info("endEdit");
+            }
+        };
+
+        return exports;
+    }
+);
