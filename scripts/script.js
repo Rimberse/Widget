29d7ac81057e0c5920fc795866414c6da9399c7f
Rimberse <kerimpltm@gmail.com>
2022-05-05 14:51:19 +0200

    Change

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ console.log("Hello World!");
 
 // Loading other modules within the main script
 requirejs(["helper/util"], function(util) {
-    console.log("Loaded!");
+    widget.body.innerHTML = "Redacted!";
 });
